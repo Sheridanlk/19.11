0d51a5938fff108963c9d8075d1e893e021d94c9
Alexandr Kobzev <sasha_kobzev18@mail.ru>
2025-11-19 04:11:49 +0300

    feature: graceful shutdown

--- cmd/main.go
+++ cmd/main.go
@@ -1,6 +1,10 @@
 package main
 
 import (
+	"log/slog"
+	"os"
+	"os/signal"
+	"syscall"
 	"web-server/internal/config"
 	"web-server/internal/logger"
 	"web-server/internal/service"
@@ -10,8 +14,19 @@ func main() {
 	cfg := config.Load()
 	log := logger.SetupLogger(cfg.Env)
 
+	log.Info("Starting service", slog.String("env", cfg.Env))
+
 	service := service.New(log, cfg.Data_path, cfg.HTTPServer.Address, cfg.HTTPServer.Timeout, cfg.HTTPServer.Timeout, cfg.HTTPServer.IdleTimeout)
+
 	go func() {
 		service.Start()
 	}()
+
+	stop := make(chan os.Signal, 1)
+	signal.Notify(stop, syscall.SIGTERM, syscall.SIGINT)
+
+	<-stop
+
+	service.Stop()
+	log.Info("Service stopped")
 }
